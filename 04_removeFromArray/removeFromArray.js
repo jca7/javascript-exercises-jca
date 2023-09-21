15dbcffba076aa370e76531f9970c86566533127
Jesse Chan <j.chan092@gmail.com>
2023-09-21 16:26:27 -0700

    finished removeFromArray challenge

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(setOfNumbers, ...numbersToRemove) {
 
-};
+  for (const number of numbersToRemove) {
+    for(let i = 0; i < setOfNumbers.length; i++) {
+      if (number === setOfNumbers[i]) {
+        setOfNumbers.splice([i], 1);
+      } 
+    }
+  } 
+  return setOfNumbers;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
