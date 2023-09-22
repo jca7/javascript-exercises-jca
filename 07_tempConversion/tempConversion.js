ffc55b6b250feec02e76f303b704c63f396970d8
Jesse Chan <j.chan092@gmail.com>
2023-09-21 21:48:35 -0700

    finished temperature converter. quick google on how to round to 1 deciminal point.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let finalTemp = (tempInF - 32) / 1.8;
+  let roundedTemp = Math.round(finalTemp * 10) / 10;
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  let finalTemp = (tempInC * 1.8) + 32; 
+  let roundedTemp = Math.round(finalTemp * 10) / 10;
+  return roundedTemp;
 };
 
 // Do not edit below this line
