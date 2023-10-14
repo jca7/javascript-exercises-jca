acade5a7277024d6344099f972d67a44f2cc9667
Jesse Chan <j.chan092@gmail.com>
2023-10-14 13:03:52 -0700

    Finished palindrome

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,48 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b; 
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let result = 0; 
+  for (let i = 0; i < a.length; i++) {
+    result += a[i];
+  }
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  for (let i = 1; i < a.length; i++) {
+    a[0] *= a[i];
+  }
+ return a[0]
+}
 
-};
 
-const power = function() {
-	
-};
+const power = function(a, b) {
+  let finalNumber = 1; 
+  for(let i = 0; i < b; i++) {
+     finalNumber = a * finalNumber;
+  }
+  return finalNumber;
+}
+;
 
-const factorial = function() {
-	
-};
+const factorial = function(n) {
+  if (n <= 1) {
+    return 1; 
+  }
+  else {
+    for (let i = n - 1; i >= 1; i--) { 
+    n *= i; 
+    }
+  return n
+ }
+}
+
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+  let aToArray = a.split('')
+  const onlyLettersOrNums = aToArray.filter(isLetterOrNum).join('').toLowerCase(); 
+  const revStr = [...onlyLettersOrNums].reverse().join('').toLowerCase();
+  if (onlyLettersOrNums == revStr) {
+    return true;
+  } else {
+     return false;
+  }
 };
+
+
+function isLetterOrNum(char) {
+return /^[a-zA-Z0-9]$/.test(char);
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
